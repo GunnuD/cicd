83ab20395b3c27f8aa89fdf044ba3f0f58e988d8
GunnuD <38685931+GunnuD@users.noreply.github.com>
2021-08-21 17:42:36 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+var http = require('http');
+ http.createServer(function (req, res) {
+   res.writeHead(200, {'Content-Type': 'text/html'});
+   res.end('Welcome To DSPA Family! And Happy Raksha Bandhan to All :)');
+ }).listen(3030);
